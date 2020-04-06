9e825b0fc2ea7d689715fe7cd3c4eb01dad3b752
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-06 19:56:12 -0300

    :sparkles: add accordion navigation on faq
    before starting the list accordion, it is being checked if there are items inside the list

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  const classActive = "active";
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("active");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add(classActive);
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("active");
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove(classActive);
+      });
+      tabContent[index].classList.add(classActive);
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("active");
   }
-
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(index);
-    });
-  });
 }
+
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const classActive = "active";
+  if (accordionList.length) {
+    accordionList[0].classList.add(classActive);
+    accordionList[0].nextElementSibling.classList.add(classActive);
+
+    function activeAccordion() {
+      this.classList.toggle(classActive);
+      this.nextElementSibling.classList.toggle(classActive);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
