ba72920d97dd9d4178119787ccd0b3621e5c2577
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-04-08 18:41:34 -0300

    :sparkles: animation on scroll
    when scrolling the sections of the site will be animated

--- script.js
+++ script.js
@@ -74,3 +74,28 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimateScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    function animateScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf;
+        if (isSectionVisible < 0) {
+          section.classList.add("active");
+        } else {
+          section.classList.remove("active");
+        }
+      });
+    }
+    animateScroll();
+
+    window.addEventListener("scroll", animateScroll);
+  }
+}
+
+initAnimateScroll();
